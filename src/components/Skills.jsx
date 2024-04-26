f0e72fc0ea7bc1c5c5b62884d23ff1c08debf280
Sayan Das <sayandas61668@gmail.com>
2024-04-26 14:07:23 +0530

    decode

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -56,7 +56,7 @@ const Skills = () => {
         </div>
         <div className="lg:ml-auto xl:w-[38rem] mt-4">
           {/* / */}
-          <div className="  relative left-1/2 flex md:w-[32rem] w-[21rem] aspect-square rounded-full -translate-x-1/2 scale:75 md:scale-100 -top-10 md:top-0  md:mt-10 lg:mt-6 mt-20">
+          <div className="  relative left-1/2 flex md:w-[32rem] w-[22.1rem] aspect-square rounded-full -translate-x-1/2 scale:75 md:scale-100 -top-10 md:top-0  md:mt-10 lg:mt-6 mt-20">
             
               <Program className={""} />
             
